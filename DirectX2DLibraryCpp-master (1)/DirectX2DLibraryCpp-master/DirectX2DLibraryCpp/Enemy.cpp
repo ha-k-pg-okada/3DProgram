a2d9d3613ef848d61ed3f3d1a2e1edfd225f45ca
MSI\Miokiti Jr <mioneko115@gmail.com>
2020-10-11 20:47:11 +0900

    左右上下に行動するようにした。

--- DirectX2DLibraryCpp-master (1)/DirectX2DLibraryCpp-master/DirectX2DLibraryCpp/Enemy.cpp
+++ DirectX2DLibraryCpp-master (1)/DirectX2DLibraryCpp-master/DirectX2DLibraryCpp/Enemy.cpp
@@ -2,13 +2,14 @@
 #include "Enemy.h"
 
 int Counter = 0;
-int Counter1 = 0;
 Vec2 Position(200.0f, 200.0f);
 int step = 0;
-//bool a = false;
 
 void  EnemyMovement()
 {
+	Engine::LoadTexture("Enemy", "Res/Enemy.png");
+	Engine::DrawTexture(Position.X, Position.Y, "Enemy");
+
 
 	Counter++;
    
@@ -46,7 +47,6 @@ void  EnemyMovement()
 	else if(Counter <= 280)
 	{
 		step = 0;
-		//Counter = 0;
 	}
 	else if(Counter <= 320)
 	{
@@ -55,13 +55,44 @@ void  EnemyMovement()
 	else if(Counter <= 360)
 	{
 		step = 0;
+		
+	}
+	else if (Counter <= 400)
+	{
+		step = 4;
+	}
+	else if (Counter <= 440)
+	{
+		step = 0;
+	}
+	else if (Counter <= 480)
+	{
+		step = 3;
+	}
+	else if(Counter <= 520)
+	{
+		step = 0;
+	}
+	else if(Counter <= 560)
+	{
+		step = 2;
+	}
+	else if(Counter <= 600)
+	{
+		step = 0;
+	}
+	else if (Counter <= 640)
+	{
+		step = 1;
+	}
+	else if(Counter <= 680)
+	{
+		step = 0;
 		Counter = 0;
 	}
 
 
-	Engine::DrawTexture(Position.X, Position.Y, "Enemy");
 	
-	Engine::LoadTexture("Enemy", "Res/Enemy.png");
 
 	switch (step)
 	{
@@ -78,13 +109,16 @@ void  EnemyMovement()
 		
 		break;
 	case 3:
-		Position.X += 2.0f;
+		Position.Y -= 2.0f;
 		
 		break;
 
 	case 4:
-		Position.X -= 2.0f;
+		Position.Y += 2.0f;
 
+		break;
+
+	case 5: Position.Y += 2.0f;
 		break;
 	}
 
